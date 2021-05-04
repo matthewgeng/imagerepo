c91ec57132e770bae7bf93489547592482352f6d
matthewgeng <matthewgeng2@gmail.com>
2021-05-04 03:07:12 -0400

    optimize and refactor, delete useless and fix frontend axios baseurl bug

--- backend/old/eda.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('ipip/data-final.csv')
-
-df = df['Country']
-
-print(df.head())
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 // setting axios baseurl depending on environment variable
 if (process.env.REACT_APP_PROD === "true") {
-  if (window.location.hostname === "mldevapp.mdc.cginet") {
+  if (window.location.hostname === process.env.REACT_APP_HOST_NAME) {
     axios.defaults.baseURL = `https://${process.env.REACT_APP_HOST_NAME}:${process.env.REACT_APP_PROXY}`;
   } else {
     axios.defaults.baseURL = `https://${process.env.REACT_APP_HOST_IP}:${process.env.REACT_APP_PROXY}`;
